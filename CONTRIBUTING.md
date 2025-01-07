84e2c561e86630bcb067ee86cbabac19268dc781
zhoulf1006 <zhoulf1006@gmail.com>
2025-01-07 17:38:05 +0800

    init repo

---
+++ CONTRIBUTING.md
@@ -0,0 +1,63 @@
+# Contributions
+
+Thank you for your interest in contributing git-secrets scanning on GitHub Actions workflow! You will help people to build up codes with security.
+This document is a guide to help you through the process of contributing.
+
+## Become a contributor
+
+You can contribute in several ways. Here are some example:
+
+- Contribute to the codebase
+- Report and triage bugs
+- Write document for user and contributors
+- Help others by answering questions
+- Help PR review
+
+## Contribute to the codebase
+
+1. Visit [open issues](https://github.com/aloong-actions/teams-deploy-card/issues?q=is%3Aopen) to look for the feature/bugfix you want to contribute. (If the feature/bugfix doesn't exist, you can submit a case for it.)
+
+2. Confirm with repo owners about the change(feature/bugfix) you want make
+
+3. Fork and clone the repository
+
+4. Create a new branch
+   The branch name should be:</br>
+   - feature/xxx
+   - bugfix/xxx
+
+    ```bash
+    git checkout -b <my-branch-name>
+    ```
+
+5. Setup development environment:
+
+    - Local:
+
+        ```bash
+        npm run ncc
+        ```
+
+6. Make your changes
+
+7. Test your changes
+
+8. Push to your fork and submit a pull request
+
+## Report bugs
+
+Before submitting a new issue, try to make sure someone hasn't already reported the problem. Look through the [existing issues](https://github.com/aloong-actions/teams-deploy-card/issues) for similar issues.
+
+Report a bug by submitting a [bug ticket](https://github.com/aloong-actions/teams-deploy-card/issues/new?assignees=&labels=kind%2Fbug&template=bug_report.md&title=). Make sure that you provide as much information as possible on how to reproduce the bug.
+
+Follow the issue template and add additional information that will help us replicate the problem.
+
+## Suggest feature request
+
+If you have an idea of how to improve this workflow, submit a [feature request](https://github.com/aloong-actions/teams-deploy-card/issues/new?assignees=&labels=kind%2Fenhancement&template=feature_request.md&title=)
+
+## Answering questions
+
+You are welcome to 1).help other by answering their questions; 2).look for existing answer to your questions; 3).ask your question through [existing issues](https://github.com/aloong-actions/teams-deploy-card/issues).
+
+It's important to us to help these users, and we'd love your help.
---
+++ src/layouts/complete.ts
@@ -0,0 +1,125 @@
+import { getInput, warning, info } from "@actions/core";
+import * as github from "@actions/github";
+import yaml from "yaml";
+
+import { escapeMarkdownTokens, renderActions } from "../utils";
+import { Fact, PotentialAction } from "../models";
+import { formatCozyLayout } from "./cozy";
+
+export function formatFilesToDisplay(
+  files: any[],
+  allowedLength: number,
+  htmlUrl: string
+) {
+  const filesChanged = files
+    .slice(0, allowedLength)
+    .map(
+      (file: any) =>
+        `[${escapeMarkdownTokens(file.filename)}](${file.blob_url}) (${
+          file.changes
+        } changes)`
+    );
+
+  let filesToDisplay = "";
+  if (files.length === 0) {
+    filesToDisplay = "*No files changed.*";
+  } else {
+    filesToDisplay = "* " + filesChanged.join("\n\n* ");
+    if (files.length > 7) {
+      const moreLen = files.length - 7;
+      filesToDisplay += `\n\n* and [${moreLen} more files](${htmlUrl}) changed`;
+    }
+  }
+
+  return filesToDisplay;
+}
+
+export function formatCompleteLayout(
+  commit: any,
+  conclusion: string,
+  elapsedSeconds?: number
+) {
+  const repoUrl = `${github.context.serverUrl}/${process.env.GITHUB_REPOSITORY}`;
+  const branchUrl = `${repoUrl}/tree/${process.env.GITHUB_REF}`;
+  const webhookBody = formatCozyLayout(commit, conclusion, elapsedSeconds);
+  const section = webhookBody.sections[0];
+
+  // for complete layout, just replace activityText with potentialAction
+  section.activityText = undefined;
+  section.potentialAction = renderActions(
+    `${repoUrl}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+    commit.data.html_url
+  );
+
+  // Set status and elapsedSeconds
+  let labels = `\`${conclusion.toUpperCase()}\``;
+  if (elapsedSeconds) {
+    labels = `\`${conclusion.toUpperCase()} [${elapsedSeconds}s]\``;
+  }
+
+  // const committer = commit.data.author.login.replace(/-/g, "_")
+  const actor = process.env.GITHUB_ACTOR || ""
+  // Set section facts
+  section.facts = [
+    new Fact(
+      "Event type:",
+      "`" + process.env.GITHUB_EVENT_NAME?.toUpperCase() + "`"
+    ),
+    new Fact("Status:", labels),
+    new Fact("Actor:", actor.replace(/-/g, "_")),
+    new Fact(
+      "Commit message:",
+      escapeMarkdownTokens(commit.data.commit.message)
+    ),
+    new Fact("Repository & branch:", `[${branchUrl}](${branchUrl})`),
+  ];
+
+  // Set custom facts
+  const customFacts = getInput("custom-facts");
+  if (customFacts && customFacts.toLowerCase() !== "null") {
+    try {
+      let customFactsCounter = 0;
+      const customFactsList = yaml.parse(customFacts);
+      if (Array.isArray(customFactsList)) {
+        (customFactsList as any[]).forEach((fact) => {
+          if (fact.name !== undefined && fact.value !== undefined) {
+            section.facts?.push(new Fact(fact.name + ":", fact.value));
+            customFactsCounter++;
+          }
+        });
+      }
+      info(`Added ${customFactsCounter} custom facts.`);
+    } catch {
+      warning("Invalid custom-facts value.");
+    }
+  }
+
+  // Set environment name
+  const environment = getInput("environment");
+  if (environment !== "") {
+    section.facts.splice(
+      1,
+      0,
+      new Fact("Environment:", `\`${environment.toUpperCase()}\``)
+    );
+  }
+
+  // Set list of files
+  if (getInput("include-files").toLowerCase() === "true") {
+    const allowedFileLen = getInput("allowed-file-len").toLowerCase();
+    const allowedFileLenParsed = parseInt(
+      allowedFileLen === "" ? "7" : allowedFileLen
+    );
+    const filesToDisplay = formatFilesToDisplay(
+      commit.data.files,
+      allowedFileLenParsed,
+      commit.data.html_url
+    );
+    section.facts?.push({
+      name: "Files changed:",
+      value: filesToDisplay,
+    });
+  }
+
+  return webhookBody;
+}
